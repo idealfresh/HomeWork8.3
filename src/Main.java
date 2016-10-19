6eac9ae6d81966bf8617fa74751b294f8ef5636f
owner90 <owner90@192.168.5.9>
2016-10-19 12:07:26 +0300

    Method odd,even

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+
+public class Main {
+	public static Scanner in = new Scanner(System.in);
+	
+	
+	
+	public static void f1(int a) {
+		if (a % 2 == 0) {
+			System.out.println(a + " - Even number");
+		} else {
+			System.out.println(a + " - Odd number");
+		}
+	}
+	
+	}
